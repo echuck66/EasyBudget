be36e81c486b060d994ec7d064d09f4755792a05
Charlie Crawford <crawfordnet@icloud.com>
2018-01-31 21:27:13 -0500

    * AboutAssets.txt: Added Application Projects (Android, iOS, Mac   Desktop)
    * EasyBudget.sln:
    * Main.cs:
    * Main.cs:
    * Info.plist:
    * Info.plist:
    * Document.cs:
    * AppDelegate.cs:
    * AppDelegate.cs:
    * Main.storyboard:
    * Main.storyboard:
    * ViewController.cs:
    * ViewController.cs:
    * Entitlements.plist:
    * Entitlements.plist:
    * MainActivity.cs:
    * packages.config:
    * EasyBudget.Mac.csproj:
    * EasyBudget.iOS.csproj:
    * LaunchScreen.storyboard:
    * ViewController.designer.cs:
    * ViewController.designer.cs:
    * EasyBudget.Android.csproj:
    * Contents.json:
    * Contents.json:
    * Main.axml:
    * AssemblyInfo.cs:
    * AboutResources.txt:
    * Strings.xml:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * Icon.png:
    * Icon.png:
    * Icon.png:
    * Icon.png:
    * Icon.png:
    * Contents.json:
    * Contents.json:
    * AppIcon-32.png:
    * AppIcon-16.png:
    * AppIcon-256.png:
    * AppIcon-512.png:
    * AppIcon-128.png:
    * AppIcon-16@2x.png:
    * AppIcon-32@2x.png:
    * AppIcon-256@2x.png:
    * AppIcon-512@2x.png:
    * AppIcon-128@2x.png:

---
+++ EasyBudget.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace EasyBudget.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
