5b3ea9ee7f12e21c3972f9347e157d5221531b74
Charlie Crawford <crawfordnet@icloud.com>
2018-02-04 16:41:12 -0500

    * EasyBudgetDataService.cs: More Navigation coding. Navigation to   Budget Items and Edit/View BudgetItem object
    * Main.storyboard:
    * EasyBudget.iOS.csproj:
    * BudgetItem.cs:
    * IncomeItem.cs:
    * ExpenseItem.cs:
    * BudgetItemType.cs:
    * EditBudgetItemViewController.cs:
    * EditCategoryTabBarController.cs:
    * ViewBudgetItemViewController.cs:
    * ViewCategoryTabBarController.cs:
    * BudgetItemsTableViewController.cs:

--- EasyBudget.iOS/ViewControllers/EditBudgetItemViewController.cs
+++ EasyBudget.iOS/ViewControllers/EditBudgetItemViewController.cs
@@ -1,13 +1,17 @@
 using Foundation;
 using System;
 using UIKit;
+using EasyBudget.Models.DataModels;
 
 namespace EasyBudget.iOS
 {
     public partial class EditBudgetItemViewController : UIViewController
     {
+        public BudgetItem BudgetItem { get; set; }
+
         public EditBudgetItemViewController (IntPtr handle) : base (handle)
         {
+            
         }
     }
 }
--- EasyBudget.iOS/ViewControllers/EditCategoryTabBarController.cs
+++ EasyBudget.iOS/ViewControllers/EditCategoryTabBarController.cs
@@ -11,6 +11,7 @@ namespace EasyBudget.iOS
 
         public EditCategoryTabBarController (IntPtr handle) : base (handle)
         {
+            this.NavigationItem.RightBarButtonItem = new UIBarButtonItem("New", UIBarButtonItemStyle.Plain, OnNewItemClicked);
         }
 
         public override void ViewDidLoad()
@@ -24,7 +25,24 @@ namespace EasyBudget.iOS
                 {
                     (vcCategory as EditBudgetCategoryViewController).Category = this.Category;
                 }
+                var vcItems = controllers[1];
+                if (vcItems != null)
+                {
+                    (vcItems as BudgetItemsTableViewController).CategoryId = this.Category.id;
+                }
             }
+        }
+
+        /// <summary>
+        /// Event handler for New button in Navigation Bar. Used to notify
+        /// the ViewController via the controlling UITabBarController
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">e</param>
+        private void OnNewItemClicked(Object sender, EventArgs e)
+        {
+            var budgetItemVC = this.Storyboard.InstantiateViewController("EditBudgetItemViewController");
+            this.NavigationController.PushViewController(budgetItemVC, true);
         }
     }
 }
